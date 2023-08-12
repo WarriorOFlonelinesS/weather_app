ea05803cea1140945d7ab81dba007596aa9fd419
WarriorOFlonelinesS <ydneprovskiy@gmail.com>
2023-08-12 16:19:04 +0300

    Added switching beetwen contents

--- js/app.js
+++ js/app.js
@@ -1,53 +1,66 @@
 const weatherBlock = document.getElementById('weather');
-const submit = document.getElementById('submit')
-const form = document.getElementById('form')
+const submit = document.getElementById('submit');
+const form = document.getElementById('form');
+const buttons = document.querySelectorAll('.main__btn');
+const contents = document.querySelectorAll('.weather-content');
 
-form.addEventListener('submit', loadWeather)
-async function loadWeather(e){
-    e.preventDefault()
+buttons.forEach((btn,index) =>{
+  btn.addEventListener('click',()=>{
+    buttons.forEach(btn => {btn.classList.remove('_active')});
+    btn.classList.add('_active');
+    contents.forEach(content =>{content.classList.remove('active')});
+    contents[index].classList.add('active')
+  })
+  }
+)
 
-    var apiKey = 'bb0808955e9c9cac837e8cbdf68de3a8';
-    const formData = new FormData(form);
+// form.addEventListener('submit', loadWeather)
+// async function loadWeather(e){
+//     e.preventDefault()
 
-  let city = formData.get('city')
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}&lang=en`;
+//     var apiKey = 'bb0808955e9c9cac837e8cbdf68de3a8';
+//     const formData = new FormData(form);
 
-  const response = await fetch(url, {method:'GET',
-} );
-    const responseResult = await response.json();
+//   let city = formData.get('city')
+//   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&units=metric&appid=${apiKey}&lang=en`;
 
-    if(response.ok){
-        getWeather(responseResult, city);
+//   const response = await fetch(url, {method:'GET',
+// } );
+//     const responseResult = await response.json();
 
-    }else{
-        weatherBlock.innerHTML = responseResult.message;
-    }
-}
+//     if(response.ok){
+//         getWeather(responseResult, city);
+
+//     }else{
+//         weatherBlock.innerHTML = responseResult.message;
+//     }
+// }
 
 
-function getWeather(data, nameOfCity){
-  console.log(data)
-    const location = nameOfCity;
-    const temp = Math.round(data.main.feels_like);
-    const feelsLike = Math.round(data.main.feels_like);
-    const weatherStatus = data.weather[0].main;
-    const weatherIcon = data.weather[0].icon;
+// function getWeather(data, nameOfCity){
+//   console.log(data)
+//     const location = nameOfCity;
+//     const temp = Math.round(data.main.feels_like);
+//     const feelsLike = Math.round(data.main.feels_like);
+//     const weatherStatus = data.weather[0].main;
+//     const weatherIcon = data.weather[0].icon;
 
-    const template = `
-    <div class='weather-holder'> 
-    <h3 class='weather__title'> Today </h3>
- </div>
-<div class="weather__main">
-    <div class="weather__header">
+//     const template = `
+//     <div class='weather-holder'> 
+//     <h3 class='weather__title'> Today </h3>
+//  </div>
+// <div class="weather__main">
+//     <div class="weather__header">
  
-      <div class="weather__city">${location}</div>
-      <div class="weather__status">${weatherStatus}</div>
-    </div>
-    <div class="weather__icons">
-      <img src="https://openweathermap.org/img/w/${weatherIcon}.png" alt="">
-    </div>
-  </div>
-  <div class="weather__temp">${temp}</div>
-  <div class="wether__feels-like">${feelsLike}</div>`
-  weatherBlock.innerHTML = template;
-}
+//       <div class="weather__city">${location}</div>
+//       <div class="weather__status">${weatherStatus}</div>
+//     </div>
+//     <div class="weather__icons">
+//       <img src="https://openweathermap.org/img/w/${weatherIcon}.png" alt="">
+//     </div>
+//   </div>
+//   <div class="weather__temp">${temp}</div>
+//   <div class="wether__feels-like">${feelsLike}</div>`
+//   weatherBlock.innerHTML = template;
+// }
+
